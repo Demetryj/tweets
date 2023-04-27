5bd698efda7236cc065a7b3070135473e209f093
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-27 20:46:32 +0300

    Create useUsers.js

---
+++ src/components/hooks/useUsers.js
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import {
+  selectUsers,
+  selectIsLoading,
+  selectError,
+} from "../../redux/users/selectors";
+
+export const useUsers = () => {
+  const users = useSelector(selectUsers);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  return { users, isLoading, error };
+};
