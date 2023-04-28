063503b29c32852d36c2fc2a9ec960dfc053d2c6
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-28 20:27:54 +0300

    Create ListUserCard.jsx

---
+++ src/components/ListTweetCard/ListUserCard.jsx
@@ -0,0 +1,17 @@
+import { useUsers } from '../../hooks/useUsers';
+import { UserCard } from '../TweetCard/UserCard';
+import css from './ListUserCard.module.css';
+
+export const ListUserCard = () => {
+  const { users } = useUsers();
+
+  return (
+    <ul className={css.userList}>
+      {users.map(user => (
+        <li className={css.userItem} key={user.id}>
+          <UserCard dataUser={user} />
+        </li>
+      ))}
+    </ul>
+  );
+};
