e6161f0ad9d7052291f44e46733b2a42a061f3a7
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-29 18:19:08 +0300

    Update Tweets.jsx

--- src/pages/Tweets.jsx
+++ src/pages/Tweets.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getUsers } from '../redux/users/operations';
+import { ListUserCard } from '../components/ListUserCard';
+
 const Tweets = () => {
-  return <div>Tweets</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  return (
+    <main>
+      <section>
+        <ListUserCard />
+      </section>
+    </main>
+  );
 };
 
 export default Tweets;
