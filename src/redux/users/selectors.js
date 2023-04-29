e89a1644c03ca147ca635f9025109ed4e32697f5
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-30 00:36:28 +0300

    Update selectors.js
    add selector selectPage

--- src/redux/users/selectors.js
+++ src/redux/users/selectors.js
@@ -5,3 +5,5 @@ export const selectIsLoading = state => state.users.isLoading;
 export const selectError = state => state.users.error;
 
 export const selectFollowing = state => state.users.following;
+
+export const selectPage = state => state.users.page;
