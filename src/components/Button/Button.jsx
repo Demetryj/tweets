11a9dd89b9c01b38cfc80a60ff16b3c30615ea6b
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-29 18:12:50 +0300

    Update Button.jsx
    add props and className with clsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,19 @@
+import clsx from 'clsx';
 import css from './Button.module.css';
 
-export const Button = ({ type = 'button', children, variant }) => {
+export const Button = ({
+  type = 'button',
+  children,
+  variant,
+  onClick,
+  following,
+}) => {
   return (
-    <button className={css[variant]} type={type}>
+    <button
+      className={clsx(css[variant], { [css.isFollowing]: following })}
+      type={type}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
