5f8bfda82e499915cf911fa9ece9fe32f15eab82
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-30 00:37:54 +0300

    Update operations.js
    add request's params for pagination

--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -1,13 +1,13 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = "https://639a14a416b0fdad7753a72e.mockapi.io/api/v1";
+axios.defaults.baseURL = 'https://639a14a416b0fdad7753a72e.mockapi.io/api/v1';
 
 export const getUsers = createAsyncThunk(
-  "users/getUsers",
-  async (_, thunkAPI) => {
+  'users/getUsers',
+  async (page, thunkAPI) => {
     try {
-      const response = await axios.get("/users");
+      const response = await axios.get(`/users?page=${page}&limit=6`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
