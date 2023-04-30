8bc16fddc6690aba9de64e49c4e87a6cbed6c8a1
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-01 00:10:03 +0300

    Update selectors.js
    add selector for visible users

--- src/redux/users/selectors.js
+++ src/redux/users/selectors.js
@@ -1,3 +1,7 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { selectFilter } from '../filters/selectors';
+import { statusFilters } from '../filters/constatnts';
+
 export const selectUsers = state => state.users.items;
 
 export const selectIsLoading = state => state.users.isLoading;
@@ -7,3 +11,24 @@ export const selectError = state => state.users.error;
 export const selectFollowing = state => state.users.following;
 
 export const selectPage = state => state.users.page;
+
+export const selectVisibleUsers = createSelector(
+  [selectUsers, selectFollowing, selectFilter],
+
+  (users, following, status) => {
+    switch (status) {
+      case statusFilters.followings:
+        return users.filter(user =>
+          following.some(item => item.id === user.id)
+        );
+
+      case statusFilters.follow:
+        return users.filter(user =>
+          following.every(item => item.id !== user.id)
+        );
+
+      default:
+        return users;
+    }
+  }
+);
