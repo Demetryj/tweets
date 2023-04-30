6732120188c907e82250fb4bf8590010f99d8e50
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-01 00:09:16 +0300

    Update operations.js
    change  key value "limit"

--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -5,9 +5,9 @@ axios.defaults.baseURL = 'https://639a14a416b0fdad7753a72e.mockapi.io/api/v1';
 
 export const getUsers = createAsyncThunk(
   'users/getUsers',
-  async (page, thunkAPI) => {
+  async ({ page, limit }, thunkAPI) => {
     try {
-      const response = await axios.get(`/users?page=${page}&limit=6`);
+      const response = await axios.get(`/users?page=${page}&limit=${limit}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
