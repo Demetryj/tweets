c7f8ac2d26b3fc72de55f069b206571e5518ac04
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-01 00:05:32 +0300

    Update ListUserCard.jsx
    add user`s filters

--- src/components/ListUserCard/ListUserCard.jsx
+++ src/components/ListUserCard/ListUserCard.jsx
@@ -3,11 +3,11 @@ import { UserCard } from '../UserCard';
 import css from './ListUserCard.module.css';
 
 export const ListUserCard = () => {
-  const { users } = useUsers();
+  const { visibleUsers } = useUsers();
 
   return (
     <ul className={css.userList}>
-      {users.map(user => (
+      {visibleUsers.map(user => (
         <li className={css.userItem} key={user.id}>
           <UserCard dataUser={user} />
         </li>
